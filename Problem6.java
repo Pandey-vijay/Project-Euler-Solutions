e57da4933c74cbe10280433437cf3143ba5a8fec
Pandey-vijay <vp1734@gmail.com>
2020-06-01 16:34:37 +0530

    Problem 6 complete

--- Problem6.java
+++ Problem6.java
@@ -1,5 +1,13 @@
 public class Problem6 {
     public static void main(String[] args) {
-        
+        int range = 100;
+        int sumSquare = 0;
+        int squareSum = 0;
+        for(int i =1;i<=range;i++){
+            sumSquare += i;
+            squareSum += i*i;
+        }
+        sumSquare*=sumSquare;
+        System.out.println(sumSquare-squareSum);
     }
 }
