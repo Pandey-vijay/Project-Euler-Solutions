a08bf9a3569e811777b8aee49dc4bc3fa96ef997
Pandey-vijay <vp1734@gmail.com>
2020-06-02 20:15:56 +0530

    Problem 9

--- Problem9.java
+++ Problem9.java
@@ -1,7 +1,17 @@
 public class Problem9 {
     public static void main(String[] args) {
-        int a=1,b=1,c=1;
-        
-        
+        int a ,b ,c;
+        for(a =0;a<=1000;a++){
+            for(b=0;b<=1000;b++){
+                for(c=0;c<=1000;c++){
+                    if(a+b+c == 1000){
+                       if((a*a)+(b*b)==(c*c))
+                        {
+                            System.out.println(a*b*c);
+                        }            
+                    }
+                }
+            }
+        }
     }
 }
